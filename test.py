1cbec023057aad97438e1f8a367a2951a15fdff4
teknasd <teknas97@gmail.com>
2023-11-07 23:03:11 +0530

    dcm to dcm compression

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from compression import HTJ2K
+from htj2k.compression import HTJ2K
 
-com = HTJ2K("./data/CT0.npy")
+# com = HTJ2K("./data/CT0.npy")
+com = HTJ2K("./data/AA002.dcm")
 encode_time = com.compress()
 print("encode_time",encode_time)
